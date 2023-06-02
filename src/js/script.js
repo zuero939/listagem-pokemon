312c765a2a3b63ffa0c032b2f68352261c329c24
zuero939 <122842727+zuero939@users.noreply.github.com>
2023-06-02 02:46:44 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -23,6 +23,7 @@ botaoAlterarTema.addEventListener("click", () => {
     const modoEscuroAtivo = body.classList.contains("modo-escuro");
     if (modoEscuroAtivo) {
         body.classList.remove("modo-escuro");
+        trocaImagemDoTema.setAttribute("src", "./src/imagens/sun.png");
         } else {
             body.classList.add("modo-escuro");
 
